a4edd32099b936b067493ae0bb00ea6d7f245359
monarcode <ugbahisioma@gmail.com>
2025-11-23 14:14:46 +0100

    refactor(ui): add squircle treatment to more components for ui consistency

--- app/components/ui/input-group.tsx
+++ app/components/ui/input-group.tsx
@@ -15,8 +15,9 @@ function InputGroup({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       className={cn(
-        "group/input-group border-input dark:bg-input/30 relative flex w-full items-center rounded-md border shadow-xs transition-[color,box-shadow] outline-none",
-        "h-9 min-w-0 has-[>textarea]:h-auto",
+        "group/input-group border-input dark:bg-input/30 relative flex w-full items-center border shadow-xs transition-[color,box-shadow] outline-none",
+        "h-10 min-w-0 has-[>textarea]:h-auto",
+        "squircle-rounded-2xl",
 
         // Variants based on alignment.
         "has-[>[data-align=inline-start]]:[&>input]:pl-2",
